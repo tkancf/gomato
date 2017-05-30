618f1dd16ba1cb4a23c11d9f86fbe792f73b0d65
tkancf <tkancf@users.noreply.github.com>
2017-05-30 19:20:57 +0900

    Add JSON save function

--- gomato.go
+++ gomato.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	//	"reflect"
 	"strconv"
 	"sync"
 	"time"
@@ -12,6 +15,18 @@ import (
 	"github.com/sethgrid/multibar"
 	"github.com/urfave/cli"
 )
+
+type Task struct {
+	Name    string `json:"name"`
+	State   string `json:"state"`
+	Date    string `json:"date"`
+	Time    string `json:"time"`
+	Elapsed int    `json:"Elapsed"`
+}
+
+type Tasks []Task
+
+var data Task
 
 func main() {
 	err := termbox.Init()
@@ -34,6 +49,11 @@ func commands() {
 			Usage:   "Start pomodoro timer\n\tYou can set time [task] [short break] [long break]\n\t(default time is 25 5 15)",
 			Action:  timerAction,
 		},
+		{
+			Name:   "test",
+			Usage:  "test command",
+			Action: saveData,
+		},
 	}
 	app.After = func(c *cli.Context) error {
 		fmt.Println("END")
@@ -43,53 +63,59 @@ func commands() {
 }
 
 func timerAction(c *cli.Context) {
+	if !fileExists("./data.json") {
+		ioutil.WriteFile("./data.json", []byte(""), os.ModePerm)
+	}
 	fmt.Println("'ESC' Key to Quit")
 	cu, _ := curse.New()
 	c0 := "25"
 	c1 := "5"
 	c2 := "15"
-	if c.NArg() == 3 {
-		c0 = c.Args().Get(0)
-		c1 = c.Args().Get(1)
-		c2 = c.Args().Get(2)
+	data.Name = "unknown"
+
+	if c.NArg() == 4 {
+		data.Name = c.Args().Get(0)
+		c0 = c.Args().Get(1)
+		c1 = c.Args().Get(2)
+		c2 = c.Args().Get(3)
+	} else if c.NArg() == 1 {
+		data.Name = c.Args().Get(0)
 	}
-	x, _ := strconv.Atoi(c0)
-	y, _ := strconv.Atoi(c1)
-	z, _ := strconv.Atoi(c2)
+	t0, _ := strconv.Atoi(c0)
+	t1, _ := strconv.Atoi(c1)
+	t2, _ := strconv.Atoi(c2)
 	for i := 1; i < 4; i++ {
-		str := "[task time] " + ":(" + strconv.Itoa(i) + "/4)" + c1 + " Second"
-		timer(x, str)
+		data.State = "task"
+		timer(t0, getTaskString(data.State, t0, i))
 		cu.MoveUp(1)
 		cu.EraseCurrentLine()
+		data.Elapsed = t0
 		checkContinue()
-		str = "[break time] " + ":(" + strconv.Itoa(i) + "/4)" + c1 + " Second"
-		timer(y, str)
+
+		data.State = "break"
+		timer(t1, getTaskString(data.State, t1, i))
 		cu.MoveUp(1)
 		cu.EraseCurrentLine()
+		data.Elapsed = t1
 		checkContinue()
 	}
-	str := "[task time] " + ":(4/4)" + c0 + " Second"
-	timer(x, str)
+	data.State = "task"
+	timer(t0, getTaskString(data.State, t0, 4))
 	cu.MoveUp(1)
 	cu.EraseCurrentLine()
+	data.Elapsed = t0
 	checkContinue()
-	timer(z, "[long break time] "+c2+" Second")
-}
 
-func checkContinue() {
-	fmt.Printf("'ENTER' to Continue")
-	switch ev := termbox.PollEvent(); ev.Type {
-	case termbox.EventKey:
-		if ev.Ch == 'q' {
-			os.Exit(1)
-		} else if ev.Key == termbox.KeyEnter {
-			// No Action
-		}
-	}
+	data.State = "lbreak"
+	timer(t2, getTaskString(data.State, t2, 0))
+	data.Elapsed = t2
+	saveFile(saveData())
 }
 
 // t = 経過時間, s = 左に表示するステータス
 func timer(t int, s string) {
+	data.Time = getTime()
+	data.Date = getDate()
 	progressBars, _ := multibar.New()
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
@@ -98,12 +124,60 @@ func timer(t int, s string) {
 	go progressBars.Listen()
 	go func() {
 		for i := 0; i <= t; i++ {
+			data.Elapsed = i
 			barProgress(i)
 			time.Sleep(time.Second * 1)
 		}
 		wg.Done()
 	}()
 	wg.Wait()
+}
+
+func getTime() string {
+	t := time.Now()
+	const layout = "15:04"
+	ts := t.Format(layout)
+	return ts
+}
+
+func getDate() string {
+	t := time.Now()
+	const layout = "2006-01-02"
+	ts := t.Format(layout)
+	return ts
+}
+
+// タイマー終了後の継続確認
+func checkContinue() {
+	fmt.Printf("'ENTER' to Continue")
+	saveData()
+	switch ev := termbox.PollEvent(); ev.Type {
+	//case termbox.EventKey:
+	//	if ev.Ch == 'q' {
+	//		saveFile(saveData())
+	//		os.Exit(1)
+	//	} else if ev.Key == termbox.KeyEnter {
+	//	}
+	}
+}
+
+func getTaskString(s string, t int, i int) string {
+	ts := strconv.Itoa(t)
+	is := strconv.Itoa(i)
+	str := ""
+	if s == "task" {
+		data.State = s
+		str = data.Name + "[task time]" + "(" + is + "/4)<" + ts + ">"
+	} else if s == "break" {
+		data.State = s
+		str = "[break time]" + "(" + is + "/4)<" + ts + ">"
+	} else if s == "lbreak" {
+		data.State = s
+		str = "[long break time]<" + ts + ">"
+	} else {
+		return str
+	}
+	return str
 }
 
 func handleKeyEvent() {
@@ -113,8 +187,49 @@ func handleKeyEvent() {
 			if ev.Key == termbox.KeyEsc {
 				os.Exit(1)
 			} else if ev.Ch == 'q' {
+				saveFile(saveData())
 				os.Exit(1)
 			}
 		}
 	}
 }
+
+//大体出来てるあとステータスを手に入れてワタしたげるだけ
+func saveData() Tasks {
+	var s Tasks
+	s = getJson("./data.json")
+	s = append(s, Task{Name: data.Name, State: data.State, Date: data.Date, Time: data.Time, Elapsed: data.Elapsed})
+	return s
+}
+
+func saveFile(s Tasks) {
+	writeJson("./data.json", &s)
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+func getJson(p string) Tasks {
+	var t Tasks
+	raw, err := ioutil.ReadFile(p)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	json.Unmarshal(raw, &t)
+	return t
+}
+
+func writeJson(p string, t *Tasks) {
+	json, err := json.Marshal(t)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	ioutil.WriteFile(p, json, os.ModePerm)
+}
+
+//func getStatus() string, string, string, string{
+//}
